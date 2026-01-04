784c04ed5589bd9f131ae517824f1e55c17264f7
DIVAKAR-0110 <rdivakar0110@gmail.com>
2026-01-04 08:01:09 +0530

    Update DJANGO_SETTINGS_MODULE path in asgi.py

--- backend/ssretails/ssretails/asgi.py
+++ backend/ssretails/ssretails/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ssretails.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ssretails.ssretails.settings')
 
 application = get_asgi_application()
